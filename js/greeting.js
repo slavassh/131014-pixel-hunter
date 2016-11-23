ca5a5318291d6308d218a09ec14849a598078895
Viacheslav Shestakov <slavassh@gmail.com>
2016-11-23 22:41:13 +0500

    Пробная декомпозиция для greetings

--- js/greeting.js
+++ js/greeting.js
@@ -5,21 +5,37 @@ import getElementFromTemplate from './getElement.js';
 import rulesElement from './rules.js';
 import insertBlock from './page.js';
 
-const greetingElement = getElementFromTemplate(
-    `<div class="greeting  central--blur">
-      <div class="greeting__logo"><img src="img/logo_big.png" width="201" height="89" alt="Pixel Hunter"></div>
-      <h1 class="greeting__asterisk">*</h1>
+const welcome = {
+  asterisk: '*',
+  content: {
+    title: 'Лучшие художники-фотореалисты бросают&nbsp;тебе&nbsp;вызов!',
+    text: `Правила игры просты.<br>
+            Нужно отличить рисунок&nbsp;от фотографии и сделать выбор.<br>
+            Задача кажется тривиальной, но не думай, что все так просто.<br>
+            Фотореализм обманчив и коварен.<br>
+            Помни, главное — смотреть очень внимательно.`
+  },
+  footer: '<div class="greeting__continue"><span><img src="img/arrow_right.svg" width="64" height="64" alt="Next"></span></div>'
+};
+
+const head = `<h1 class="greeting__asterisk">${welcome.asterisk}</h1>`;
+
+const content = `<div class="greeting__logo"><img src="img/logo_big.png" width="201" height="89" alt="Pixel Hunter"></div>
       <div class="greeting__challenge">
-        <h3>Лучшие художники-фотореалисты бросают&nbsp;тебе&nbsp;вызов!</h3>
-        <p>Правила игры просты.<br>
-          Нужно отличить рисунок&nbsp;от фотографии и сделать выбор.<br>
-          Задача кажется тривиальной, но не думай, что все так просто.<br>
-          Фотореализм обманчив и коварен.<br>
-          Помни, главное — смотреть очень внимательно.</p>
+      <h3>${welcome.content.title}</h3>
+      <p>${welcome.content.text}</p>
+      </div>`;
+
+const footer = `<footer>${welcome.footer}</footer>`;
+
+const article = `
+      ${head}
+      <div class="greeting  central--blur">
+        ${content}
       </div>
-      <div class="greeting__continue"><span><img src="img/arrow_right.svg" width="64" height="64" alt="Next"></span></div>
-    </div>`
-);
+      ${footer}`;
+
+const greetingElement = getElementFromTemplate(article);
 
 const rulesArrow = greetingElement.querySelector('.greeting__continue');
 
