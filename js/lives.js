d0fbf5f9f1874fa2576acfa0a5910d66a937fa8a
Viacheslav Shestakov <slavassh@gmail.com>
2016-11-27 13:36:18 +0500

    По замечаниям: функция для вывода количества жизней

--- js/lives.js
+++ js/lives.js
@@ -1,16 +1,23 @@
 /**
  * Created by slavash on 25.11.2016.
  */
-const lives = {
-  empty: 'img/heart__empty.svg',
-  full: 'img/heart__full.svg'
+let livesCount = 2;
+const MAX_LIVES = 3;
+
+const emptyHeartIcon = 'img/heart__empty.svg';
+const fullHeartIcon = 'img/heart__full.svg';
+
+const lives = () => {
+  let hearts = '';
+  for (let i = MAX_LIVES; i > 0; i--) {
+    hearts += `<img src="${ livesCount < i ? emptyHeartIcon : fullHeartIcon}" class="game__heart" alt="Life" width="32" height="32">`;
+  }
+  return hearts;
 };
 
 const lifebar = `
       <div class="game__lives">
-        <img src="${lives.empty}" class="game__heart" alt="Life" width="32" height="32">
-        <img src="${lives.full}" class="game__heart" alt="Life" width="32" height="32">
-        <img src="${lives.full}" class="game__heart" alt="Life" width="32" height="32">
+        ${lives()}
       </div>`;
 
 export default lifebar;
