af0d52db0389e0753f0293c2d62ecde6984d68d6
Viacheslav Shestakov <slavassh@gmail.com>
2016-12-05 21:27:52 +0500

    Оптимизирована структура

--- js/data/game-data.js
+++ js/data/game-data.js
@@ -1,147 +1,143 @@
 export const game = {
-  questions: {
-    'task-1': 'Угадайте для каждого изображения фото или рисунок?',
-    'task-2': 'Угадай, фото или рисунок?',
-    'task-3': 'Найдите рисунок среди изображений'
-  },
-  answers: {
-    photo: 'Фотография',
-    paint: 'Рисунок'
-  }
+  questions: [
+    'Угадайте для каждого изображения фото или рисунок?',
+    'Угадай, фото или рисунок?',
+    'Найдите рисунок среди изображений'
+  ]
 };
 
 export const screens = {
   'screen-0': {
-    task: game.questions['task-1'],
+    task: game.questions[0],
     answers: [
       {
         image: 'http://placehold.it/468x458',
-        correct: game.answers.photo
+        correct: 'photo'
       },
       {
         image: 'http://placehold.it/468x458',
-        correct: game.answers.paint
+        correct: 'paint'
       }
     ]
   },
   'screen-1': {
-    task: game.questions['task-2'],
+    task: game.questions[1],
     answers: [
       {
         image: 'http://placehold.it/705x455',
-        correct: game.answers.paint
+        correct: 'paint'
       }
     ]
   },
   'screen-2': {
-    task: game.questions['task-3'],
+    task: game.questions[2],
     answers: [
       {
         image: 'http://placehold.it/304x455',
-        correct: game.answers.photo
+        correct: 'photo'
       },
       {
         image: 'http://placehold.it/304x455',
-        correct: game.answers.photo
+        correct: 'photo'
       },
       {
         image: 'http://placehold.it/304x455',
-        correct: game.answers.paint
+        correct: 'paint'
       },
     ]
   },
   'screen-3': {
-    task: game.questions['task-1'],
+    task: game.questions[0],
     answers: [
       {
         image: 'http://placehold.it/468x458',
-        correct: game.answers.photo
+        correct: 'photo'
       },
       {
         image: 'http://placehold.it/468x458',
-        correct: game.answers.paint
+        correct: 'paint'
       }
     ]
   },
   'screen-4': {
-    task: game.questions['task-2'],
+    task: game.questions[1],
     answers: [
       {
         image: 'http://placehold.it/705x455',
-        correct: game.answers.paint
+        correct: 'paint'
       }
     ]
   },
   'screen-5': {
-    task: game.questions['task-3'],
+    task: game.questions[2],
     answers: [
       {
         image: 'http://placehold.it/304x455',
-        correct: game.answers.photo
+        correct: 'photo'
       },
       {
         image: 'http://placehold.it/304x455',
-        correct: game.answers.photo
+        correct: 'photo'
       },
       {
         image: 'http://placehold.it/304x455',
-        correct: game.answers.paint
+        correct: 'paint'
       },
     ]
   },
   'screen-6': {
-    task: game.questions['task-1'],
+    task: game.questions[0],
     answers: [
       {
         image: 'http://placehold.it/468x458',
-        correct: game.answers.photo
+        correct: 'photo'
       },
       {
         image: 'http://placehold.it/468x458',
-        correct: game.answers.paint
+        correct: 'paint'
       }
     ]
   },
   'screen-7': {
-    task: game.questions['task-2'],
+    task: game.questions[1],
     answers: [
       {
         image: 'http://placehold.it/705x455',
-        correct: game.answers.paint
+        correct: 'paint'
       }
     ]
   },
   'screen-8': {
-    task: game.questions['task-3'],
+    task: game.questions[2],
     answers: [
       {
         image: 'http://placehold.it/304x455',
-        correct: game.answers.photo
+        correct: 'photo'
       },
       {
         image: 'http://placehold.it/304x455',
-        correct: game.answers.photo
+        correct: 'photo'
       },
       {
         image: 'http://placehold.it/304x455',
-        correct: game.answers.paint
+        correct: 'paint'
       },
     ]
   },
   'screen-9': {
-    task: game.questions['task-3'],
+    task: game.questions[2],
     answers: [
       {
         image: 'http://placehold.it/304x455',
-        correct: game.answers.photo
+        correct: 'photo'
       },
       {
         image: 'http://placehold.it/304x455',
-        correct: game.answers.photo
+        correct: 'photo'
       },
       {
         image: 'http://placehold.it/304x455',
-        correct: game.answers.paint
+        correct: 'paint'
       },
     ]
   }
--- js/templates/questions.js
+++ js/templates/questions.js
@@ -7,59 +7,57 @@ const gameContainer = document.createElement('div');
 gameContainer.classList.add('game');
 
 const question = (data) => {
-  if (data.task === game.questions['task-1']) {
+  if (data.task === game.questions[0]) {
+    const question1 = () => {
+      let tpl = '';
+      for (let i = 0; i < data.answers.length; i++) {
+        tpl += `<div class="game__option">
+                  <img src="${data.answers[i].image}" alt="Option ${i}" width="468" height="458">
+                  <label class="game__answer game__answer--photo">
+                    <input name="question${i}" type="radio" value="photo">
+                    <span>Фотография</span>
+                  </label>
+                  <label class="game__answer game__answer--paint">
+                    <input name="question${i}" type="radio" value="paint">
+                    <span>Рисунок</span>
+                  </label>
+                </div>`;
+      }
+      return tpl;
+    };
     gameContainer.innerHTML = `<p class="game__task">${data.task}</p>
           <form class="game__content">
-            <div class="game__option">
-              <img src="${data.answers[0].image}" alt="Option 1" width="468" height="458">
-              <label class="game__answer game__answer--photo">
-                <input name="question1" type="radio" value="photo">
-                <span>${data.answers[0].photo}</span>
-              </label>
-              <label class="game__answer game__answer--paint">
-                <input name="question1" type="radio" value="paint">
-                <span>${data.answers[0].paint}</span>
-              </label>
-            </div>
-            <div class="game__option">
-              <img src="${data.answers[1].image}" alt="Option 2" width="468" height="458">
-              <label class="game__answer  game__answer--photo">
-                <input name="question2" type="radio" value="photo">
-                <span>${data.answers[1].photo}</span>
-              </label>
-              <label class="game__answer  game__answer--paint">
-                <input name="question2" type="radio" value="paint">
-                <span>${data.answers[1].paint}</span>
-              </label>
+            ${question1()}
             </div>
           </form>`;
-  } else if (data.task === game.questions['task-2']) {
+  } else if (data.task === game.questions[1]) {
     gameContainer.innerHTML = `<p class="game__task">${data.task}</p>
             <form class="game__content  game__content--wide">
               <div class="game__option">
                 <img src="${data.answers[0].image}" alt="Option 1" width="705" height="455">
                 <label class="game__answer  game__answer--photo">
                   <input name="question1" type="radio" value="photo">
-                  <span>${data.answers[0].photo}</span>
+                  <span>Фотография</span>
                 </label>
                 <label class="game__answer  game__answer--wide  game__answer--paint">
                   <input name="question1" type="radio" value="paint">
-                  <span>${data.answers[0].paint}</span>
+                  <span>Рисунок</span>
                 </label>
               </div>
             </form>`;
-  } else if (data.task === game.questions['task-3']) {
+  } else if (data.task === game.questions[2]) {
+    const question3 = () => {
+      let tpl = '';
+      for (let i = 0; i < data.answers.length; i++) {
+        tpl += `<div class="game__option">
+                  <img src="${data.answers[i].image}" alt="Option ${i}" width="304" height="455">
+                </div>`;
+      }
+      return tpl;
+    };
     gameContainer.innerHTML = `<p class="game__task">${data.task}</p>
             <form class="game__content  game__content--triple">
-              <div class="game__option">
-                <img src="${data.answers[0].image}" alt="Option 1" width="304" height="455">
-              </div>
-              <div class="game__option  game__option--selected">
-                <img src="${data.answers[1].image}" alt="Option 1" width="304" height="455">
-              </div>
-              <div class="game__option">
-                <img src="${data.answers[2].image}" alt="Option 1" width="304" height="455">
-              </div>
+              ${question3()}
             </form>`;
   }
   return gameContainer;
