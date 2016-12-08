991df8e1d22e0f52d59bc6759ad25169051ca9e7
Viacheslav Shestakov <slavassh@gmail.com>
2016-12-08 16:46:47 +0500

    По замечаниям 4: добавлены тесты

--- js/data/process.js
+++ js/data/process.js
@@ -5,7 +5,7 @@ import {gameState} from './game-data.js';
 
 export const setScreen = (game, question) => {
   if (question < 0 || question >= gameState.screens.length) {
-    throw new RangeError(`This game has only ${gameState.screens.length} questions`);
+    throw new RangeError(`screenNumber ${gameState.screenNumber} is out of range of screens`);
   }
   return Object.assign({}, game, {
     screenNumber: question
@@ -14,22 +14,22 @@ export const setScreen = (game, question) => {
 
 export const getScreen = (num) => gameState.screens[num];
 
-export const setTime = (game, time) => {
-  if (time < 0) {
-    throw new RangeError('Time can not be negative');
-  }
-  return Object.assign({}, game, {
-    time: time
-  });
-};
-
 export const setLives = (game, lives) => {
-  if (lives < 0) {
-    throw new RangeError('Number of lives can not be negative');
+  if (lives < 0 || lives > gameState.livesCount) {
+    throw new RangeError(`livesCount = ${gameState.livesCount}. livesCount can't be negative or > max number of lives`);
   }
 
   return Object.assign({}, game, {
     livesCount: lives
+  });
+};
+
+export const setTime = (game, time) => {
+  if (time < 0 || time > 30) {
+    throw new RangeError(`Time ${gameState.time} is out of time period`);
+  }
+  return Object.assign({}, game, {
+    time: time
   });
 };
 
--- js/data/process.test.js
+++ js/data/process.test.js
@@ -1,22 +1,49 @@
 /**
  * Created by slavash on 01.12.2016.
  */
-import {gameState, setScreen, setLives, setTime} from './process.js';
+import {setScreen, setLives, setTime} from './process.js';
+import {gameState} from './game-data.js';
 import assert from 'assert';
 
 describe('setScreen', () => {
   it('should throws an error if negative value passed', () => {
     assert.throws(() => setScreen(gameState, -1));
   });
-  it('should throws an error if Number of screens > 10', () => {
-    assert.throws(() => setScreen(gameState, 10));
+  it('should throws an error if value more then number of screens', () => {
+    gameState.screens = [{}, {}, {}];
+    assert.throws(() => setScreen(gameState, gameState.screens.length));
+  });
+  it('should change screenNumber in game stage', () => {
+    let state = setScreen(gameState, 2);
+    let expectedState = Object.assign({}, gameState, {screenNumber: 2});
+    assert.deepEqual(state, expectedState);
   });
 });
+
 describe('setLives', () => {
   it('should throws an error if Number of lives < 0', () => {
     assert.throws(() => setLives(gameState, -1));
   });
+  it('should throws an error if value more then max number of lives', () => {
+    assert.throws(() => setLives(gameState, gameState.livesCount + 1));
+  });
+  it('should change livesCount in game stage', () => {
+    let state = setLives(gameState, 2);
+    let expectedState = Object.assign({}, gameState, {livesCount: 2});
+    assert.deepEqual(state, expectedState);
+  });
+});
+
+describe('setTime', () => {
   it('should throws an error if time < 0', () => {
     assert.throws(() => setTime(gameState, -1));
+  });
+  it('should throws an error if value more then response time', () => {
+    assert.throws(() => setTime(gameState, gameState.time + 1));
+  });
+  it('should change time in game stage', () => {
+    let state = setTime(gameState, 5);
+    let expectedState = Object.assign({}, gameState, {time: 5});
+    assert.deepEqual(state, expectedState);
   });
 });
